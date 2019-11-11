1056189d5f2a3e3112a09b3e6e25ebf926be7212
vfon98 <vfon98@gmail.com>
2019-11-11 10:47:03 +0700

    Updates

---
+++ precache-manifest.61110aee54a9c725be63aa3b05036b39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18c259c93518a2512a103da1a9a9188",
+    "url": "/TodoList-ReactJs_Redux/index.html"
+  },
+  {
+    "revision": "722d5049f230e4a7ee91",
+    "url": "/TodoList-ReactJs_Redux/static/css/main.c6967736.chunk.css"
+  },
+  {
+    "revision": "0792ef2e7ff0437f5154",
+    "url": "/TodoList-ReactJs_Redux/static/js/2.2df760a9.chunk.js"
+  },
+  {
+    "revision": "722d5049f230e4a7ee91",
+    "url": "/TodoList-ReactJs_Redux/static/js/main.a28bbbd2.chunk.js"
+  },
+  {
+    "revision": "daf20d7b17a6610a4ea7",
+    "url": "/TodoList-ReactJs_Redux/static/js/runtime-main.772c40eb.js"
+  }
+]);
